933b52c645ece463516d1505ce808d8cd60c386c
dickyfd <dickyfajardarmawan@gmail.com>
2020-04-22 11:53:01 +0700

    adfsfd

---
+++ bk-listlaporan-ui/src/main/java/bk/listlaporan/ui/constants/BkListlaporanUiPortletKeys.java
@@ -0,0 +1,10 @@
+package bk.listlaporan.ui.constants;
+
+/**
+ * @author 0452
+ */
+public class BkListlaporanUiPortletKeys {
+
+	public static final String BkListlaporanUi = "bklistlaporanui";
+
+}
